0e409659a23d8a5064e7e6902ec80edf3bed2b07
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-11-13 20:55:42 +0100

    Add comments to Basket component for product filtering and display logic

--- src/app/components/Basket.jsx
+++ src/app/components/Basket.jsx
@@ -4,6 +4,9 @@ import useStore from "../store/basketStore";
 const Basket = () => {
   const { basketProducts, addToBasket } = useStore();
 
+//   1.Filtrer på de individuelle produkter i kurven for at få længden af arrayet (som er kvantiteten)
+//   2.Vis denne længde i BasketButton komponenten ved siden af "Kurv" teksten
+// 3. Lav en const DisplayProducts som er basketProducts forkortet til kun at vise hvert individuelt produkt 1 gang
   return (
     <section>
       <ul>
